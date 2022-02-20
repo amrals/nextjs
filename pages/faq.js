35784b31bf9f9096945199d730459a977209c555
amrals <matheus.a.c.c.l@gmail.com>
2022-02-20 16:00:20 -0300

    'up'

---
+++ pages/faq.js
@@ -0,0 +1,10 @@
+import Link from '../src/components/Link'
+
+export default function Homepage(){
+    return(
+        <div>
+            <h1>Alura Cases - FAQ</h1>
+            <Link href="/">Ir para a Home</Link>
+        </div>
+    )
+}
